e165d2204f628ade6f1017252ebb160f6eb5a892
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-21 07:44:19 +0400

    Add platform movement

--- platform.lua
+++ platform.lua
@@ -5,11 +5,19 @@ function Platform:load()
     self.height = 20
     self.x = love.graphics:getWidth() / 2 - self.width / 2
     self.y = love.graphics:getHeight() - 100
-    self.speed = 200
+    self.speed = 500
 end
 
 function Platform:update(dt)
+    self:move(dt)
+end
 
+function Platform:move(dt)
+    if love.keyboard.isDown("d", "right") then
+        self.x = self.x + self.speed * dt
+    elseif love.keyboard.isDown("a", "left") then
+        self.x = self.x - self.speed * dt
+    end
 end
 
 function Platform:draw()

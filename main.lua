f9da0eb8ac7917c1a8003ae1973098a6d7560e8b
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-21 09:53:11 +0400

    Added walls on the screen

--- main.lua
+++ main.lua
@@ -1,13 +1,24 @@
 local Player = require("platform")
+local Wall = require("wall")
+
+local SCREEN_WIDTH = love.graphics:getWidth()
+local SCREEN_HEIGHT = love.graphics:getHeight()
 
 function love.load()
     Player:load()
+
+    --walls
+    Wall.new(0, 0, SCREEN_WIDTH, 10)
+    Wall.new(0, 0, 10, SCREEN_HEIGHT)
+    Wall.new(SCREEN_WIDTH-10, 0, 10, SCREEN_HEIGHT)
 end
 
 function love.update(dt)
     Player:update(dt)
+    Wall:update(dt)
 end
 
 function love.draw()
     Player:draw()
+    Wall.drawAll()
 end

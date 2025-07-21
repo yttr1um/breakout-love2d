bbd7900f82533d0ff3763164395001555b9ad1cb
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-21 11:37:21 +0400

    Added ball

---
+++ ball.lua
@@ -0,0 +1,21 @@
+local Ball = {}
+
+local platform = require("platform")
+
+function Ball:load()
+    self.x = love.graphics.getWidth() / 2
+    self.y = platform.y - 20
+    self.radius = 10
+end
+
+function Ball:update(dt)
+
+end
+
+function Ball:draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", self.x, self.y, self.radius)
+    love.graphics.setColor(1, 1, 1)
+end
+
+return Ball
--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local Player = require("platform")
 local Wall = require("wall")
+local Ball = require("ball")
 
 local SCREEN_WIDTH = love.graphics:getWidth()
 local SCREEN_HEIGHT = love.graphics:getHeight()
@@ -8,6 +9,7 @@ function love.load()
     world = love.physics.newWorld(0, 0)
 
     Player:load()
+    Ball:load()
 
     --walls
     Wall.new(0, 0, SCREEN_WIDTH, 10)
@@ -18,10 +20,12 @@ end
 function love.update(dt)
     world:update(dt)
     Player:update(dt)
+    Ball:update(dt)
     Wall:update(dt)
 end
 
 function love.draw()
     Player:draw()
+    Ball:draw()
     Wall.drawAll()
 end

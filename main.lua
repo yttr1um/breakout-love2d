00f05d27a11a55e532055c914afeacd888f7f6a6
Salem Alhaddad <85304055+yttr1um@users.noreply.github.com>
2025-07-24 17:11:53 +0400

    removed walls

--- main.lua
+++ main.lua
@@ -1,31 +1,22 @@
 local Player = require("platform")
-local Wall = require("wall")
 local Ball = require("ball")
 
 local SCREEN_WIDTH = love.graphics:getWidth()
 local SCREEN_HEIGHT = love.graphics:getHeight()
 
 function love.load()
-
     Player:load()
     Ball:load()
-
-    --walls
-    Wall.new(0, 0, SCREEN_WIDTH, 10)
-    Wall.new(0, 0, 10, SCREEN_HEIGHT)
-    Wall.new(SCREEN_WIDTH-10, 0, 10, SCREEN_HEIGHT)
 end
 
 function love.update(dt)
     Player:update(dt)
     Ball:update(dt)
-    Wall:update(dt)
 end
 
 function love.draw()
     Player:draw()
     Ball:draw()
-    Wall.drawAll()
 end
 
 function love.keypressed(key)
